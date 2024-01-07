1c84f934e19be7cacd610203ea511d579d9ddfa7
erifonseca <150080706+erifonseca@users.noreply.github.com>
2024-01-07 15:44:37 -0300

    Criando as aulas JavaScript

---
+++ IntroducaoWeb/Aula_JavaScript/script01.js
@@ -0,0 +1,66 @@
+console.log("Hello World");
+document.write("Hello World");
+alert("Hello World");
+
+
+console.log("Jose Erinaldo da Fonseca");
+console.log("Desenvolvedor e Físico");
+console.log("São Paulo");
+document.write(nome);
+document.write("<br>");
+document.write(profissao);
+document.write("<br>");
+document.write(cidade);
+
+/* Variáveis */
+/* const tipoNumber = 42;
+const tipoNumber2 = 1.60;
+
+const tipoString = 'Hello, World!'; 
+const tipoString2 = "Hello, World!";
+console.log(tipoString, tipoString2);
+
+const tipoBoolean = true;
+const tipoBoolean2 = false;
+
+let tipoUndefined;
+let x;
+
+const tipoNull = null;
+
+const tipoSymbol = Symbol('mySymbol');
+
+const pessoa = {
+    nome: 'João Ninguém',
+    idade: 26,
+    cidade: 'Campinas',
+  }; */
+
+  // Exemplo de Array com elementos
+/* const numeros = [1, 2, 3, 4, 5]; */
+
+// Exemplo de Array com tipos diferentes
+/* const arrayMisturado = [1, 'dois', true, null, { nome: 'João' }];
+
+const tipoNaN = NaN;
+const tipoNaN2 = 10 / 'texto';
+console.log(tipoNaN2);
+
+var nomeCompleto = "Jose Erinaldo da Fonseca";
+let senhaBanco = 123456;
+const numero_pi = 3.14; */
+
+/* Intereção - comando de entrada */
+
+/* var nome = prompt("Qual o seu nome?");
+var profissao = prompt("Qual a sua profissão?");
+var cidade = prompt("Qual a sua cidade?");
+
+console.log(nome);
+console.log(profissao);
+console.log(cidade); */
+
+
+  
+
+
