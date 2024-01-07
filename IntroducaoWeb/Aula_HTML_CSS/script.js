cbe7c3cbca4f788af0c1f18f283f251c446077b9
erifonseca <150080706+erifonseca@users.noreply.github.com>
2024-01-07 15:46:02 -0300

    Criando as Aulas HMTL e CSS

---
+++ IntroducaoWeb/Aula_HTML_CSS/script.js
@@ -0,0 +1,19 @@
+
+console.log("Hello World");
+document.write("Hello World");
+
+
+/* var ana = 10;
+var Ana = 20;
+console.log(ana == Ana);
+console.log(ana == ana);
+console.log(Ana == Ana); */
+
+
+
+
+
+
+
+
+
